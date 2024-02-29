7f1e65189481b9ea8c53daeaa07dd7f15fa9a02c
mehran-starcode <mehran2bb@gmail.com>
2024-02-29 14:39:08 +0330

    sdf

--- djangoAPI/djangoAPI/settings.py
+++ djangoAPI/djangoAPI/settings.py
@@ -98,10 +98,18 @@ WSGI_APPLICATION = 'djangoAPI.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'website',
+		'USER': 'mehran',
+		'PASSWORD': 'netcarshow/Net23@',
+		'HOST':'127.0.0.1',
+		'PORT':'3306',
+		'OPTIONS': {
+        'charset': 'utf8mb4',
+        'init_command': 'ALTER DATABASE website DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci',
     }
+	}
 }
 
 
